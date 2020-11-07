8c8162fdd3cf3c1c986a4e13db28345c49693f59
Jose Alfredo <josalbm94@gmail.com>
2020-11-06 19:26:29 -0600

    Quitamos el metodo main para que no cause ruido en el codigo. Si se desea ver un ejemplo de invocacion, ver las clases de de testing.

--- java/ImageMaker/src/main/java/mx/ryo/xoloit/utils/image/maker/QRMaker.java
+++ java/ImageMaker/src/main/java/mx/ryo/xoloit/utils/image/maker/QRMaker.java
@@ -1,13 +1,9 @@
 package mx.ryo.xoloit.utils.image.maker;
 
 import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.util.EnumMap;
 import java.util.Map;
-
-import javax.imageio.ImageIO;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -20,26 +16,6 @@ import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 public class QRMaker {
-	
-	public static void main(String[] args) {
-		Image imgQr;
-		try {
-			imgQr = QRMaker.generarQRCode("Hola mundo", 250);
-			BufferedImage bi = (BufferedImage) imgQr;
-			File outputfile = new File("/swtools/Temp/saved.png");
-
-			ImageIO.write(bi, "png", outputfile);
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (WriterException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 	/**
 	 * 
 	 * @param encodeTxt
